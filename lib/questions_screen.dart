c53da14d4d609162999aec1ba75bf8ff2effe501
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-21 15:24:07 +0200

    import & use google fonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -32,8 +33,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 189, 167, 217),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
               textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
             ),
             SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
