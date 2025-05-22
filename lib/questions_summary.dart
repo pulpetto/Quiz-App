8f702373c30c569b3dbfdf05ddf9bde653dd7968
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-22 15:17:49 +0200

    make summary scrollable

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,25 +7,30 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:
-          summaryData.map((data) {
-            return Row(
-              children: [
-                Text(((data['question-index'] as int) + 1).toString()),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(data['question'] as String),
-                      SizedBox(height: 5),
-                      Text(data['user-answer'] as String),
-                      Text(data['correct-answer'] as String),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          }).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children:
+              summaryData.map((data) {
+                return Row(
+                  children: [
+                    Text(((data['question-index'] as int) + 1).toString()),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(data['question'] as String),
+                          SizedBox(height: 5),
+                          Text(data['user-answer'] as String),
+                          Text(data['correct-answer'] as String),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+        ),
+      ),
     );
   }
 }
